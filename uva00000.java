e9aa97c85c4ef4079c0cf8ad416860df9432f8e4
Matthew Worley <mworley2@illinois.edu>
2016-06-03 16:01:59 -0500

    first commit

---
+++ uva00000.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class uva00000 {
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
---
+++ uva00272.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class uva00272 {
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
