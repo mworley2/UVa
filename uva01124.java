49909739dba297118141486665b0f2065a323089
Matthew Worley <mworley2@illinois.edu>
2016-06-03 16:47:58 -0500

    2

---
+++ uva01124.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class uva01124 {
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input;
+		while((input = br.readLine()) != null) {
+			System.out.println(input);
+		}
+	}
+}
