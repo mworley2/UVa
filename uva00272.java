f5ff8c8a2faea5adfd8eddc0e1037edf2fc50f69
Matthew Worley <mworley2@illinois.edu>
2016-06-03 16:42:44 -0500

    1

--- uva00272.java
+++ uva00272.java
@@ -5,6 +5,29 @@ public class uva00272 {
 	public static void main(String args[]) throws Exception
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean isLeft = true;
+		String input;
+		while((input = br.readLine())!= null) {
+			char[] buff = input.toCharArray();
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i<buff.length; i++) {
+				if(buff[i] == '"') {
+					if(isLeft) {
+						sb.append("`");
+						sb.append("`");
+						isLeft = false;
+					}
+					else {
+						sb.append("'");
+						sb.append("'");
+						isLeft = true;
+					}
+				}
+				else
+				sb.append(buff[i]);
+			}
+			System.out.println(sb.toString());
+		}
 		
 	}
 
