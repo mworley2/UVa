3857ea0a07f9ac75bb92414d2b807d3a22834e9c
Matthew Worley <mworley2@illinois.edu>
2016-06-19 18:21:46 -0500

    29

---
+++ uva11933.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <bitset>
+
+using namespace std;
+int main() {
+    int n;
+    while(scanf("%d", &n) == 1 && n != 0) {
+        bitset<32> bin(n);
+        bitset<32> a(0);
+        bitset<32> b(0);
+        bool flipA = true;
+        for(int i=0; i<32; i++) {
+            if(bin[i]) {
+                if(flipA) {
+                    a.set(i, 1);
+                    flipA = false;
+                }
+                else {
+                    b.set(i,1);
+                    flipA = true;
+                }
+            }
+        }
+        printf("%d %d\n",a.to_ulong(),b.to_ulong());
+    }
+}
